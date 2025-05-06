666ab9045869b9d6275ce2d2705304581c4a9723
aromalkallingal <aromalkallingal5@gmail.com>
2025-05-06 16:52:43 +0530

    learning

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,17 @@ import { useRef } from 'react'
 
 function App() {
 const inputElement = useRef(null)
-console.log(inputElement)
+
+const focusInput = () => {
+  inputElement.current.focus()
+  inputElement.current.value = "Aromal"
+};
+
 
   return <div>
     <input type="text" ref={inputElement} />
+    <button onClick={() => focusInput()}>Focus and write</button>
+
   </div>
 }
 

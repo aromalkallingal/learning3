f3cac67bc8e6ba17d145f407a29e8c83f0e35589
aromalkallingal <aromalkallingal5@gmail.com>
2025-05-14 17:34:01 +0530

    Learn

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
 import { useEffect, useState } from "react";
-import Post from "./components/Post";
 
 export default function App() {
 
- 
-  const [posts, setPosts] = useState([]);
-
-    const loadPosts = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await res.json();
-    setPosts(data);
-  };
-  
-    useEffect(() => {
-      loadPosts();
-    }, []);
-
   return <div>
-    {posts.map((post) => <Post title={post.title} body={post.body}/>)}
+  <p>Hello</p>
   </div>
 }
 

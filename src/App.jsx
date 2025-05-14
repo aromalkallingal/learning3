bbc6628491d7fd81c420e26ee30a13eb3fd14642
aromalkallingal <aromalkallingal5@gmail.com>
2025-05-14 15:39:15 +0530

    learn

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
+
 export default function App() {
 
+  const [data, setData] = useState([]);
   const loadPosts = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await res.text();
-    console.log(data);
-  }
+    const data = await res.json();
+    setData(data);
+  };
   return <div>
     <button onClick={loadPosts}>Load</button>
+    {data.map((post) => <h1>{post.title}</h1>)}
   </div>
 }
 
